077eca1d3d977e0242dc90bf055aabfb13f9873a
t13768 <t13768@ms.sltung.com.tw>
2023-03-14 12:05:22 +0800

    feat: storybook adding sass style: update stylesheet, use sass

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,7 +6,15 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    {
+      name: '@storybook/addon-styling',
+      options: {
+        sass: {
+          implementation: require('sass'),
+        },
+      },
+    },
   ],
   "framework": "@storybook/vue3",
   "core": {
