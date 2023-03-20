61341f0a3575b796568484c4e05a0d3f543dd449
t13768 <t13768@ms.sltung.com.tw>
2023-03-20 16:50:50 +0800

    update entry

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,7 @@
 import YoButton from './Button/YoButton.vue'
-
-import YoInputText from './Input/YoInputText.vue'
-import YoInputPassword from './Input/YoInputPassword.vue'
+import YoInput from './Input/YoInput.vue'
 
 export default {
   YoButton,
-
-  YoInputText,
-  YoInputPassword
+  YoInput
 };
